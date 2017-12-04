3e6e10c3763299177646e1bae2e36e160d48a69d
Vestein Dahl <vesteindahl@hotmail.com>
2017-12-04 04:01:23 +0100

    Updates styling.

--- src/layout/homepage/About.js
+++ src/layout/homepage/About.js
@@ -6,6 +6,7 @@ class About extends Component {
   render() {
     return (
         <div className="home_page__about">
+          <p>Placeholder</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum elit ac ultricies ullamcorper. Donec.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a pulvinar est, quis consectetur odio. Vestibulum eleifend urna ac quam.</p>
         </div>
--- src/layout/homepage/Contact.js
+++ src/layout/homepage/Contact.js
@@ -6,10 +6,10 @@ class Contact extends Component {
   render() {
     return (
         <div className="home_page__contact">
-          <a href="" title="Send me an email"><i className="fa fa-envelope">Email</i></a>
+          <a href="mailto:vesteindahl@gmail.com" title="Send me an email"><i className="fa fa-envelope">Email</i></a>
           <a href="https://github.com/WoodStone" title="Check out my Github Account!"><i className="fa fa-github">Github</i></a>
-          <i className="fa fa-linkedin">LinkedIn</i>
-          <i className="fa fa-twitter">Twitter</i>
+          <a href="https://twitter.com/vesteindahl" title="My Twitter account"><i className="fa fa-twitter">Twitter</i></a>
+          <a href="/" title="My LinkedIn profile"><i className="fa fa-linkedin">LinkedIn</i></a>
         </div>
     )
   }

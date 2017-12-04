ae51188aac39289a56d9872844590f212c7df096
Vestein Dahl <vesteindahl@hotmail.com>
2017-12-04 04:33:46 +0100

    Adds basic routing and redux store.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page from "./layout/Page";
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Card from "./layout/homepage/Card";
 
 class App extends Component {
   render() {
     return (
-      <Page/>
+
+        <Provider store={store}>
+          <Router>
+            <div>
+              <Route exact path="/" component={Page}/>
+              <Route path="/projects" component={Card}/>
+              <Route path="/blog" component={Card}/>
+              <Route path="/about" component={Card}/>
+            </div>
+          </Router>
+        </Provider>
     );
   }
 }

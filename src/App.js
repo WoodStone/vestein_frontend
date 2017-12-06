80ff333762908ce3023e6cd8dac8baf1ee8f96b9
Vestein Dahl <vesteind@stud.ntnu.no>
2017-12-06 04:18:18 +0100

    Restructures project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import Page from "./layout/Page";
-import store from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Card from "./layout/homepage/Card";
+
+import './App.css';
+import './core/flex_layout.css';
+import store from './redux/store';
+
+import Blog from "./pages/blog/Blog";
+import Card from "./pages/homepage/Card";
+import HomePage from "./pages/homepage/HomePage";
+import Projects from "./pages/projects/Projects";
 
 class App extends Component {
   render() {
@@ -12,10 +17,10 @@ class App extends Component {
 
         <Provider store={store}>
           <Router>
-            <div>
-              <Route exact path="/" component={Page}/>
-              <Route path="/projects" component={Card}/>
-              <Route path="/blog" component={Card}/>
+            <div className="vd_container_main">
+              <Route exact path="/" component={HomePage}/>
+              <Route path="/projects" component={Projects}/>
+              <Route path="/blog" component={Blog}/>
               <Route path="/about" component={Card}/>
             </div>
           </Router>

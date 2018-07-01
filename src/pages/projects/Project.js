9bea03dfc87e2197abb582bdae28b91a011b5b1e
Vestein Dahl <vesteind@stud.ntnu.no>
2018-07-01 21:43:08 +0200

    Adds simple rendering for projects.

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
+import {Card, Icon, Image} from "semantic-ui-react";
 
 class Project extends Component {
 
   render() {
-    const { name, desc, img } = this.props;
+    const { header, meta, description, glink, imagelink } = this.props;
     return (
-        <span>Project</span>
+        <Card>
+          <Image src={imagelink}/>
+          <Card.Content>
+            <Card.Header>{header}</Card.Header>
+            <Card.Meta>{meta}</Card.Meta>
+            <Card.Description>{description}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a href={glink}><Icon size={"large"} name="github"/></a>
+          </Card.Content>
+        </Card>
     )
   }
 
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react';
+import axios from "axios/index";
+import {PROJECTS} from "../../config";
+import Project from "./Project";
 
 class Projects extends Component {
 
+  componentDidMount() {
+    this.getProjects()
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      projects: null
+    }
+  }
+
+  getProjects() {
+    axios.get(PROJECTS)
+      .then(response => {
+        this.setState({projects: response.data})
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
-    return (
-        <span>Projects</span>
-    )
+    const {projects} = this.state;
+
+    if (projects != null) {
+      return (
+        <div>
+          {projects.map(p => <Project key={p.header} header={p.header} meta={"Scala"} description={p.description} glink={p.glink} imagelink={p.imagelink}/>)}
+        </div>
+      )
+    } else {
+      return (
+        <div>Waiting...</div>
+      )
+    }
   }
 
 }
